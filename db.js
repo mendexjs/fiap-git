1d959191d5a110b8b3186eb77e64a05b8c7ab93a
Marcos Carvalho <marcos.carvalho96@icloud.com>
2021-08-03 22:55:03 -0300

    Mudei o nome port para porta

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const env = require('./public/assets/environment');
 const sequelize = new Sequelize(env.environment.database.database_name, env.environment.database.user, env.environment.database.password, {
     dialect: env.environment.database.engine, 
     host: env.environment.database.host,
-    port: env.environment.database.port,
+    porta: env.environment.database.port,
 });
 
 module.exports = sequelize;
